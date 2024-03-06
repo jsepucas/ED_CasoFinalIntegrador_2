c6020c2bb8f1821e5a042ae6e88c9de897459134
jsepucas <jsepucas@myuax.com>
2024-03-06 11:43:42 +0100

    Commits

--- src/main/java/Habitats/H_Terrestres.java
+++ src/main/java/Habitats/H_Terrestres.java
@@ -1,4 +1,16 @@
 package Habitats;
 
-public class H_Terrestres {
-}
+public class HabitatTerrestre extends Habitat {
+    protected String tipoDeSuelo;
+    protected String vegetacion;  // Nueva propiedad
+
+    public HabitatTerrestre(double temperatura, double humedad, boolean estaLimpio, String tipoDeSuelo, String vegetacion) {
+        super(temperatura, humedad, estaLimpio);
+        this.tipoDeSuelo = tipoDeSuelo;
+        this.vegetacion = vegetacion;  // Inicializar la nueva propiedad
+    }
+
+    // Getter para tipoDeSuelo
+    public String getTipoDeSuelo() {
+        return tipoDeSuelo;
+    }
