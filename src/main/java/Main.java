a809de8eea87e67fb3a409de44f4c9aacf68b447
jsepucas <jsepucas@myuax.com>
2024-03-11 18:04:10 +0100

    Commits

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,23 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int option = scanner.nextInt();
 
+        switch(option){
+            case 1:
+                System.out.println("Bienvenido visitante");
+                Habitat habitat = new Habitat();
+                habitat.showAnimals();
+                break;
+            case 2:
+                System.out.println("Bienvenido empleado");
+                break;
+            case 3:
+                System.out.println("Adios");
+                break;
+            default:
+                System.out.println("Opción no válida");
+                break;
+        }
+
 
     }
 
