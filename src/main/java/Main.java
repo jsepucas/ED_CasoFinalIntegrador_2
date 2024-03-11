cb9416f099fda66efcbce7ffd5804ffe29ef2e6a
jsepucas <jsepucas@myuax.com>
2024-03-11 18:03:36 +0100

    Commits

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+import Habitats.Habitat;
+
+
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al Zoo, seleccione una opción");
+        System.out.println("1. Visitante");
+        System.out.println("2. Empleado");
+        System.out.println("3. Salir");
+        Scanner scanner = new Scanner(System.in);
+        int option = scanner.nextInt();
+
+
+    }
+
 }
