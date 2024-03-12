8ea89930a1b9c9e4c5f05d743fb30f4f90f1e131
jsepucas <jsepucas@myuax.com>
2024-03-12 20:00:24 +0100

    Commits

--- src/main/java/Animales/A_Acuaticos.java
+++ src/main/java/Animales/A_Acuaticos.java
@@ -7,4 +7,13 @@ public class A_Acuaticos extends Animal{
     protected int profundidadPreferida;
     protected String dieta;
 
+
+    public A_Acuaticos(String nombre, int edad, double peso, String estadoSalud, boolean vacunado, boolean esteril,
+                       String tipoAgua, String metodoRespiracion, int profundidadPreferida, String dieta) {
+        super(nombre, edad, peso, estadoSalud, vacunado, esteril);
+        this.tipoAgua = tipoAgua;
+        this.metodoRespiracion = metodoRespiracion;
+        this.profundidadPreferida = profundidadPreferida;
+        this.dieta = dieta;
+    }
 }
