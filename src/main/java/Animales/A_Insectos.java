8220d99d1476abb4ca6ac0c9d9baf2f0eb027c31
jsepucas <jsepucas@myuax.com>
2024-03-12 19:45:27 +0100

    Commits

--- src/main/java/Animales/A_Insectos.java
+++ src/main/java/Animales/A_Insectos.java
@@ -1,4 +1,8 @@
 package Animales;
 
-public class A_Insectos extends Animal{
-}
+public class Insecto extends Animal {
+    private int numeroPatas;
+    private boolean tieneAlas;
+    private String tipoAlimentacion;
+    private String habitat;
+    private boolean esBeneficioso;
