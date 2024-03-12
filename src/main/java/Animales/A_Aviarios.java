e5b77a61a395028a8f87a8c4768ffb4dd51cc7ee
jsepucas <jsepucas@myuax.com>
2024-03-12 19:50:24 +0100

    Commits

--- src/main/java/Animales/A_Aviarios.java
+++ src/main/java/Animales/A_Aviarios.java
@@ -8,4 +8,16 @@ public class A_Aviarios extends A_Acuaticos{
 
     private String habitatPreferido;
     private boolean esMigratoria;
+
+
+    public Ave(String nombre, int edad, double peso, String estadoSalud, boolean vacunado, boolean esteril,
+               String tipoPlumaje, boolean esCapazDeVolar, String dieta, String habitatPreferido, boolean esMigratoria) {
+        super(nombre, edad, peso, estadoSalud, vacunado, esteril);
+        this.tipoPlumaje = tipoPlumaje;
+        this.esCapazDeVolar = esCapazDeVolar;
+        this.dieta = dieta;
+        this.habitatPreferido = habitatPreferido;
+        this.esMigratoria = esMigratoria;
+    }
 }
+
