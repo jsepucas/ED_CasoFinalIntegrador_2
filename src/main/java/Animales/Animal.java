bd2437171f8fca2b7aa349cc2b3cefd15837d730
jsepucas <jsepucas@myuax.com>
2024-03-12 19:35:04 +0100

    Commits

--- src/main/java/Animales/Animal.java
+++ src/main/java/Animales/Animal.java
@@ -9,4 +9,14 @@ public abstract class Animal {
     private boolean vacunado;
     private boolean esteril;
 
+    public Animal(String nombre, int edad, double peso, String estadoSalud, boolean vacunado, boolean esteril) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+        this.estadoSalud = estadoSalud;
+        this.vacunado = vacunado;
+        this.esteril = esteril;
+    }
+
+
 }
