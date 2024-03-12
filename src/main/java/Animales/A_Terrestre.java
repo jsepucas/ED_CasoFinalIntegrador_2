bb4d48b222245fd3acc7757c9bb75004b1e4e8cb
jsepucas <jsepucas@myuax.com>
2024-03-12 19:43:16 +0100

    Commits

--- src/main/java/Animales/A_Terrestre.java
+++ src/main/java/Animales/A_Terrestre.java
@@ -6,7 +6,7 @@ public class A_Terrestre extends Animal{
         private String modoLocomocion;
         private String dieta;
 
-    public AnimalTerrestre(String nombre, int edad, double peso, String estadoSalud, boolean vacunado, boolean esteril,
+    public A_Terrestre(String nombre, int edad, double peso, String estadoSalud, boolean vacunado, boolean esteril,
                            String tipoHabitat, String modoLocomocion, String dieta) {
         super(nombre, edad, peso, estadoSalud, vacunado, esteril);
         this.tipoHabitat = tipoHabitat;
@@ -14,6 +14,10 @@ public class A_Terrestre extends Animal{
         this.dieta = dieta;
 
 
+    }
+
+    public A_Terrestre(String nombre, int edad, double peso, String estadoSalud, boolean vacunado, boolean esteril) {
+        super(nombre, edad, peso, estadoSalud, vacunado, esteril);
     }
 
     public String getTipoHabitat() {
@@ -40,5 +44,23 @@ public class A_Terrestre extends Animal{
     public void setDieta(String dieta) {
         this.dieta = dieta;
     }
+
+
+    public void alimentar() {
+        System.out.println("Alimentando al animal terrestre " + getNombre() + " con " + dieta);
+    }
+
+    @Override
+    public void realizarChequeoSalud() {
+        System.out.println("Realizando chequeo de salud al animal terrestre " + getNombre() + ". Estado de salud: " + getEstadoSalud());
+    }
+
+    public void mostrarInfoHabitat() {
+        System.out.println("El animal terrestre " + getNombre() + " vive en el hábitat de tipo " + tipoHabitat +
+                " y se mueve de manera " + modoLocomocion + ".");
+    }
+
+    // Agregar más si quiero después
+}
 
 
