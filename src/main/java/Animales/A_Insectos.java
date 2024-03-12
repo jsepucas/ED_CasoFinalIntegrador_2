f598b3a614c64d1b5d513802393762d43d85793b
jsepucas <jsepucas@myuax.com>
2024-03-12 19:45:36 +0100

    Commits

--- src/main/java/Animales/A_Insectos.java
+++ src/main/java/Animales/A_Insectos.java
@@ -6,3 +6,13 @@ public class Insecto extends Animal {
     private String tipoAlimentacion;
     private String habitat;
     private boolean esBeneficioso;
+
+    public Insecto(String nombre, int edad, double peso, String estadoSalud, boolean vacunado, boolean esteril,
+                   int numeroPatas, boolean tieneAlas, String tipoAlimentacion, String habitat, boolean esBeneficioso) {
+        super(nombre, edad, peso, estadoSalud, vacunado, esteril);
+        this.numeroPatas = numeroPatas;
+        this.tieneAlas = tieneAlas;
+        this.tipoAlimentacion = tipoAlimentacion;
+        this.habitat = habitat;
+        this.esBeneficioso = esBeneficioso;
+    }
