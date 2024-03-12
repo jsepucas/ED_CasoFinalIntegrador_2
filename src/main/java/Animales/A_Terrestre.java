ce02afac584fd2988db22eb5eab4f6840bf88bf6
jsepucas <jsepucas@myuax.com>
2024-03-12 19:41:02 +0100

    Commits

--- src/main/java/Animales/A_Terrestre.java
+++ src/main/java/Animales/A_Terrestre.java
@@ -1,4 +1,19 @@
 package Animales;
 
 public class A_Terrestre extends Animal{
-}
+
+        private String tipoHabitat;
+        private String modoLocomocion;
+        private String dieta;
+
+    public AnimalTerrestre(String nombre, int edad, double peso, String estadoSalud, boolean vacunado, boolean esteril,
+                           String tipoHabitat, String modoLocomocion, String dieta) {
+        super(nombre, edad, peso, estadoSalud, vacunado, esteril);
+        this.tipoHabitat = tipoHabitat;
+        this.modoLocomocion = modoLocomocion;
+        this.dieta = dieta;
+
+
+    }
+
+
