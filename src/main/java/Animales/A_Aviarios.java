5b0a22cbc1e6cc49c58e422b19230fb0f7e75f3d
jsepucas <jsepucas@myuax.com>
2024-03-12 19:49:30 +0100

    Commits

--- src/main/java/Animales/A_Aviarios.java
+++ src/main/java/Animales/A_Aviarios.java
@@ -1,4 +1,11 @@
 package Animales;
 
 public class A_Aviarios extends A_Acuaticos{
+
+    private String tipoPlumaje;
+    private boolean esCapazDeVolar;
+    private String dieta;
+
+    private String habitatPreferido;
+    private boolean esMigratoria;
 }
