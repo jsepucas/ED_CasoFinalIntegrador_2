7416d9c320ce13f76069eff72e3e0878328d35dc
jsepucas <jsepucas@myuax.com>
2024-03-13 20:06:13 +0100

    Commits

--- src/main/java/Habitats/H_Terrestre.java
+++ src/main/java/Habitats/H_Terrestre.java
@@ -4,8 +4,8 @@ public class H_Terrestre extends Habitat {
     protected String tipoDeSuelo;
     protected String vegetacion;  // Nueva propiedad
 
-    public H_Terrestre(double temperatura, double humedad, boolean estaLimpio, String tipoDeSuelo, String vegetacion) {
-        super(temperatura, humedad, estaLimpio);
+    public H_Terrestre(String name, double temperatura, double humedad, boolean estaLimpio, String tipoDeSuelo, String vegetacion) {
+        super(name, temperatura, humedad, estaLimpio);
         this.tipoDeSuelo = tipoDeSuelo;
         this.vegetacion = vegetacion;  // Inicializar la nueva propiedad
     }
@@ -32,9 +32,6 @@ public class H_Terrestre extends Habitat {
     //Monitoreo de las condiciones del habitat acuático
     @Override
     public void monitorearCondiciones() {
-        System.out.println("Temperatura: " + getTemperatura());
-        System.out.println("Humedad: " + getHumedad());
-        System.out.println("Esta limpio: " + isEstaLimpio());
         System.out.println("Tipo de suelo: " + getTipoDeSuelo());
         System.out.println("Vegetacion: " + getVegetacion());  //
     }
