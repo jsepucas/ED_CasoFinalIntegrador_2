c32617727b4e3f9294b1c100da875379976eb8d6
jsepucas <jsepucas@myuax.com>
2024-03-13 20:02:42 +0100

    Commits

---
+++ src/main/java/InteracciónVisitante/Q_Animales.java
@@ -0,0 +1,10 @@
+package InteracciónVisitante;
+
+public class Q_Animales extends Quiosco {
+    @Override
+    public void mostrarInformacion() {
+        super.mostrarInformacion(); // Llama al método de la clase base para mostrar el mensaje de bienvenida.
+        System.out.println("Explorando Animales: Descubre las especies más fascinantes que habitan nuestro zoológico.");
+        // Aquí podrías agregar más lógica para buscar y mostrar información específica de animales.
+    }
+}
---
+++ src/main/java/InteracciónVisitante/Q_Eventos.java
@@ -0,0 +1,10 @@
+package InteracciónVisitante;
+
+public class Q_Eventos extends Quiosco {
+    @Override
+    public void mostrarInformacion() {
+        super.mostrarInformacion(); // Mensaje de bienvenida
+        System.out.println("Eventos Especiales: Mantente al día con los eventos y actividades especiales que se llevan a cabo en el zoológico.");
+        // Implementación para mostrar información sobre eventos especiales.
+    }
+}
