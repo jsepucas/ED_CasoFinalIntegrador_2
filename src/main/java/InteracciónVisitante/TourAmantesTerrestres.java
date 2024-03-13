5041108191a7eaae0f07b685c0201e41b2345720
jsepucas <jsepucas@myuax.com>
2024-03-13 20:06:36 +0100

    Commits

---
+++ src/main/java/InteracciónVisitante/TourAmantesTerrestres.java
@@ -0,0 +1,8 @@
+package InteracciónVisitante;
+
+public class TourAmantesTerrestres implements ToursPersonalizados {
+    @Override
+    public void mostrarTour() {
+        System.out.println("Tour para amantes de animales terrestres: Encuentra a los habitantes más impresionantes de la tierra.");
+    }
+}
