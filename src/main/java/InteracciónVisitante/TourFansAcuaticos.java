8cdb465881b069e9cad1b60aef54c1b6480da9f7
jsepucas <jsepucas@myuax.com>
2024-03-13 20:06:57 +0100

    Commits

---
+++ src/main/java/InteracciónVisitante/TourFansAcuaticos.java
@@ -0,0 +1,8 @@
+package InteracciónVisitante;
+
+public class TourFansAcuaticos implements ToursPersonalizados {
+    @Override
+    public void mostrarTour() {
+        System.out.println("Tour para fans de acuáticos: Sumérgete en el mundo de los seres más increíbles del agua.");
+    }
+}
