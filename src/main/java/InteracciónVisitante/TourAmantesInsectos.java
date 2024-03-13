f3c1d7f185020ee8055104155d6c22f2918ab4d8
jsepucas <jsepucas@myuax.com>
2024-03-13 20:06:31 +0100

    Commits

---
+++ src/main/java/InteracciónVisitante/TourAmantesInsectos.java
@@ -0,0 +1,8 @@
+package InteracciónVisitante;
+
+public class TourAmantesInsectos implements ToursPersonalizados {
+    @Override
+    public void mostrarTour() {
+        System.out.println("Tour para amantes de los insectos: Explora el fascinante mundo de los insectos y su importancia para el ecosistema.");
+    }
+}
