aaae844be1e49e4edf9a6b255844432c539e47ad
jsepucas <jsepucas@myuax.com>
2024-03-13 20:06:25 +0100

    Commits

---
+++ src/main/java/InteracciónVisitante/TourAficionadosAviarios.java
@@ -0,0 +1,8 @@
+package InteracciónVisitante;
+
+public class TourAficionadosAviarios implements ToursPersonalizados {
+    @Override
+    public void mostrarTour() {
+        System.out.println("Tour para aficionados a aviarios: Descubre las aves más exóticas y su canto.");
+    }
+}
