70cc1347c2e5b2b614da6aff724606fc2c135fde
jsepucas <jsepucas@myuax.com>
2024-03-13 20:08:05 +0100

    Commits

---
+++ src/main/java/InteracciónVisitante/TourParaNiños.java
@@ -0,0 +1,8 @@
+package InteracciónVisitante;
+
+public class TourParaNiños implements ToursPersonalizados {
+    @Override
+    public void mostrarTour() {
+        System.out.println("Tour para niños: Aprende y diviértete con los animales más fascinantes.");
+    }
+}
