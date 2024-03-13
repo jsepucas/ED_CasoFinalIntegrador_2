6fc7198338299d2ba9931fbb8ac0b1ad4bb108ba
jsepucas <jsepucas@myuax.com>
2024-03-13 20:06:19 +0100

    Commits

---
+++ src/main/java/InteracciónVisitante/Q_Habitats.java
@@ -0,0 +1,10 @@
+package InteracciónVisitante;
+
+public class Q_Habitats extends Quiosco {
+    @Override
+    public void mostrarInformacion() {
+        super.mostrarInformacion(); // Mensaje de bienvenida
+        System.out.println("Explorando Hábitats: Aprende sobre los diferentes entornos que nuestros animales llaman hogar.");
+        // Implementación para mostrar información sobre hábitats.
+    }
+}
