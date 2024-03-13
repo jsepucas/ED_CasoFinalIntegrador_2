9d891aad4599e4c843d5446e502e185b8f8ab026
jsepucas <jsepucas@myuax.com>
2024-03-13 20:02:57 +0100

    Commits

---
+++ src/main/java/InteracciónVisitante/Quiosco.java
@@ -0,0 +1,8 @@
+package InteracciónVisitante;
+
+public class Quiosco {
+public void mostrarInformacion() {
+    System.out.println("Bienvenido al Quiosco Interactivo: Aquí puedes aprender sobre todos nuestros animales y hábitats.");
+}
+}
+
